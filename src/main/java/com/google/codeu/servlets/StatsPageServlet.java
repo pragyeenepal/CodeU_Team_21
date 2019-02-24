4648306590af7f2727dc281e1701b93eaf120ada
pragyeenepal <37906248+pragyeenepal@users.noreply.github.com>
2019-02-24 16:26:23 -0500

    Step 1-3

---
+++ src/main/java/com/google/codeu/servlets/StatsPageServlet.java
@@ -0,0 +1,40 @@
+package com.google.codeu.servlets;
+
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.cloud.datastore.Datastore;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;;
+
+/**
+ * Responds with a hard-coded message for testing purposes.
+ */
+
+@WebServlet("/stats")
+
+public class StatsPageServlet extends HttpServlet{
+
+	@Override
+	 public void doGet(HttpServletRequest request, HttpServletResponse response)
+	   throws IOException {
+	  
+	  response.getOutputStream().println("hello world");
+	 }
+	
+	/** Returns the total number of messages for all users. */
+	public int getTotalMessageCount(){
+      DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+	  Query query = new Query("Message");
+	  PreparedQuery results = datastore.prepare(query);
+	  return results.countEntities(FetchOptions.Builder.withLimit(1000));
+	}
+}
